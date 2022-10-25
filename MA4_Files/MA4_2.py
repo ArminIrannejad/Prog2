2773eb8958486f7e5990a6746536acb566b336f6
armino112 <armin.irannejad@gmail.com>
2022-10-25 22:13:16 +0200

    Testar numba förhoppningsvis ska det funka

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -2,6 +2,7 @@
 from person import Person
 import matplotlib as plt
 from time import perf_counter as pc
+from numba import njit
 
 def fib_py(n):
 	if n <= 1:
@@ -9,10 +10,32 @@ def fib_py(n):
 	else:
 		return (fib_py(n-1) + fib_py(n-2))
 
+@njit
+def fib_numb(n):
+	if n <= 1:
+		return n
+	else:
+		return (fib_numb(n-1) + fib_numb(n-2))
+
 		
 def main():
+	start = pc()
+	fib_py(40)
+	end = pc()
+	print(f'Normal Python took {round(end-start, 2)} seconds')
+
+	start = pc()
+	fib_numb(40)
+	end = pc()
+	print(f'Python but with numba took {round(end-start, 2)} seconds')
+
+	
 	f = Person(15)
-	print(f.Fib())
+	start = pc()
+	f.Fib()
+	end = pc()
+	print(f'Python called and compiled in C++ took {round(end-start,2)} seconds')
+
 	
 	
 
